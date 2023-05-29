16e9f98c92d9cdf41762032ff8326e23a5aca407
Junaed <junaed1995@gmail.com>
2023-05-29 13:35:35 +0600

    Initial Commit

---
+++ OnBoaedingScreen iOS/OnBoaedingScreen_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoaedingScreen_iOSApp.swift
+//  OnBoaedingScreen iOS
+//
+//  Created by Junaed Muhammad Chowdhury on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct OnBoaedingScreen_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
